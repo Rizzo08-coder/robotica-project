d15e6816526e8f98b471ca3d5945c25270b749bd
Matteo Rizzo <m.rizzo009@studenti.unibs.it>
2024-04-30 13:04:04 +0200

    add routing

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from "./routes/root";
 import reportWebVitals from './reportWebVitals';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+]);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
+   <React.StrictMode>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
