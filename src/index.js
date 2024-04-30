fd78740e8898d3c9a3fee3b051a43171131a43bb
rizzo08-coder <m.rizzo009@studenti.unibs.it>
2024-04-30 18:30:13 +0200

    routing react

--- src/index.js
+++ src/index.js
@@ -4,14 +4,19 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-import Root from "./routes/root";
+import Root from "./routes/Root";
 import reportWebVitals from './reportWebVitals';
+import './index.css'
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Root />,
+    element: <Root link="/prova" buttonText="provaaa"/>,
   },
+  {
+    path: "/prova",
+    element: <Root />
+  }
 ]);
 
 
@@ -22,7 +27,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
