5507974d5193e8ba9aa417519fe0c9d6f29e4afc
Matteo Rizzo <m.rizzo009@studenti.unibs.it>
2024-05-08 15:39:37 +0200

    graphic improvements

--- src/components/trajectories/TrajectoryElement.jsx
+++ src/components/trajectories/TrajectoryElement.jsx
@@ -12,8 +12,8 @@ function TrajectoryElement({trajectory, setDeletedTrajectory}){
                 <div className="flex items-center space-x-4 rtl:space-x-reverse">
                     <Avatar img="/trajectory.png" alt="trajectory_img" size="sm" className="w-1/6 mx-4" />
                         <div className="w-5/6 grid grid-cols-2 flex items-center">
-                            <div className=" text-sm font-medium text-gray-900 ">id = {trajectory.id}</div>
-                            <div className=" text-sm font-medium text-gray-900 mr-4">name = {trajectory.name}</div>
+                            <div className=" text-sm font-medium text-gray-900 ">id : {trajectory.id}</div>
+                            <div className=" text-md font-bold text-gray-900 mr-4">{trajectory.name}</div>
                         </div>
                     <ViewTrajectoryBtn id={trajectory.id}/>
                     <DeleteTrajectoryBtn id={trajectory.id} setDeletedTrajectory={setDeletedTrajectory}/>
